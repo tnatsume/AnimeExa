ec282c9c25a1051ee2e7c519ee4078b77d87148a
takashi <38514552+tnatsume@users.noreply.github.com>
2019-04-23 12:47:01 +0900

    1st commit!
    音楽を鳴らしながら角を丸くする

--- AnimeExa/ViewController.swift
+++ AnimeExa/ViewController.swift
@@ -7,9 +7,31 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var targetView: UIView!
+    @IBAction func tapstartButton(_ sender: Any) {
+        var soundID: SystemSoundID = 0
+        if let soundurl =  Bundle.main.url(forResource: "osaki4bansen3",withExtension:"mp3"){
+            AudioServicesCreateSystemSoundID(soundurl as CFURL,&soundID)
+            AudioServicesPlaySystemSoundWithCompletion(soundID,{()->Void in })
+            
+        }
+        
+        let animation = CABasicAnimation(keyPath:"cornerRadius")
+        animation.timingFunction = CAMediaTimingFunction(name:kCAMediaTimingFunctionDefault)
+        
+        animation.fromValue = 0
+        animation.toValue = 20
+        
+        animation.duration = 1
+        
+    targetView.layer.add(animation,forKey: "cornerRadius")
+    targetView.layer.cornerRadius = 20
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
